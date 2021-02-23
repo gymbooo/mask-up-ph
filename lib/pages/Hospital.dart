5cbb2e9103255e610000282c19d8a6210dd833a3
Katrina Anne Salvador <kate.salvador@whitecloak.com>
2021-02-23 12:34:39 +0800

    bottom panel on map marker tap

--- lib/pages/Hospital.dart
+++ lib/pages/Hospital.dart
@@ -8,16 +8,6 @@ import 'package:mask_up_ph/widgets/consts.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-Marker testMarker = Marker(
-    markerId: MarkerId('testmarker'),
-    position: LatLng(14.4098693, 121.0372225),
-    infoWindow: InfoWindow(
-        title: 'Location Name',
-        onTap: () {
-          launch('https://www.google.com');
-        }),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet));
-
 //TODO: get json, put latlng to list
 //https://maps.googleapis.com/maps/api/place/textsearch/json?query=covid+test&key=AIzaSyDmGhS77Xm9peRvlmiPYGF4vYOZQrV0ei0
 //TODO: try bottom pop up w/ loc info on marker tap (info also on json)
@@ -39,59 +29,67 @@ class _HospitalState extends State<Hospital> {
       resizeToAvoidBottomInset: false,
       // resizeToAvoidBottomPadding: false,
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: [
-            Column(
-              children: [
-                SearchMapPlaceWidget(
-                  hasClearButton: true,
-                  placeType: PlaceType.address,
-                  placeholder: 'Enter the location',
-                  apiKey: 'AIzaSyDmGhS77Xm9peRvlmiPYGF4vYOZQrV0ei0',
-                  onSelected: (Place place) async {
-                    Geolocation geolocation = await place.geolocation;
-                    mapController.animateCamera(
-                        CameraUpdate.newLatLng(geolocation.coordinates));
-                    mapController.animateCamera(
-                        CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 5.0),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height,
-                    child: GoogleMap(
-                      myLocationEnabled: true,
-                      zoomGesturesEnabled: true,
-                      zoomControlsEnabled: true,
-                      onMapCreated:
-                          (GoogleMapController googleMapController) async {
-                        Position position = await Geolocator.getCurrentPosition(
-                            desiredAccuracy: LocationAccuracy.best);
-                        currentPosition = position;
-
-                        setState(() {
-                          mapController = googleMapController;
-                          CameraPosition cameraPosition = new CameraPosition(
-                              zoom: 15.0,
-                              target: LatLng(currentPosition.latitude,
-                                  currentPosition.longitude));
-                          googleMapController.animateCamera(
-                              CameraUpdate.newCameraPosition(cameraPosition));
-                        });
-                      },
-                      initialCameraPosition: CameraPosition(
-                          zoom: 6, target: LatLng(12.8797, 121.7740)),
-                      mapType: MapType.normal,
-                      markers: {testMarker},
-                    ),
-                  ),
-                )
-              ],
+            SearchMapPlaceWidget(
+              hasClearButton: true,
+              placeType: PlaceType.address,
+              placeholder: 'Enter the location',
+              apiKey: 'AIzaSyDmGhS77Xm9peRvlmiPYGF4vYOZQrV0ei0',
+              onSelected: (Place place) async {
+                Geolocation geolocation = await place.geolocation;
+                mapController.animateCamera(
+                    CameraUpdate.newLatLng(geolocation.coordinates));
+                mapController.animateCamera(
+                    CameraUpdate.newLatLngBounds(geolocation.bounds, 0));
+              },
             ),
-            Container(
-                //cards per place
-                )
+            Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: GoogleMap(
+                    myLocationEnabled: true,
+                    zoomGesturesEnabled: true,
+                    zoomControlsEnabled: true,
+                    onMapCreated:
+                        (GoogleMapController googleMapController) async {
+                      Position position = await Geolocator.getCurrentPosition(
+                          desiredAccuracy: LocationAccuracy.best);
+                      currentPosition = position;
+
+                      setState(() {
+                        mapController = googleMapController;
+                        CameraPosition cameraPosition = new CameraPosition(
+                            zoom: 15.0,
+                            target: LatLng(currentPosition.latitude,
+                                currentPosition.longitude));
+                        googleMapController.animateCamera(
+                            CameraUpdate.newCameraPosition(cameraPosition));
+                      });
+                    },
+                    initialCameraPosition: CameraPosition(
+                        zoom: 6, target: LatLng(12.8797, 121.7740)),
+                    mapType: MapType.normal,
+                    markers: {
+                      Marker(
+                          markerId: MarkerId('testmarker'),
+                          position: LatLng(14.4098693, 121.0372225),
+                          infoWindow: InfoWindow(
+                              title: 'Location Name',
+                              onTap: () {
+                                showModalBottomSheet(
+                                    context: context,
+                                    builder: (context) {
+                                      return Text(
+                                          'bottom panel\nlocation info from json\n\n\n\n');
+                                    });
+                              }),
+                          icon: BitmapDescriptor.defaultMarkerWithHue(
+                              BitmapDescriptor.hueViolet))
+                    },
+                  ),
+                ))
           ],
         ),
       ),
